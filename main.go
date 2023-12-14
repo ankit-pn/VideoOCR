9e77cb65726e32fe2b020c99b15f14367c653ca1
ankit-pn <miankitsingh@gmail.com>
2023-12-14 20:03:11 +0530

    changes number of workers to work with system

--- main.go
+++ main.go
@@ -103,10 +103,10 @@ func indexerEngine(root_path string) {
 	})
 	
 
-	buffSize := 200
+	buffSize := 25
 	fileChan := make(chan string, buffSize)
 	errChan := make(chan error, buffSize)
-	for i := 0; i < 30; i++ {
+	for i := 0; i < 25; i++ {
         wg.Add(1)
         go worker(fileChan, rdb, ctx,errChan)
     }
